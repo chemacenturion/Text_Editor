22f8e7748ea28d5bb03b127d417cb0a6f48168fe
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-16 17:55:45 -0400

    pushing up corrected database.js w/ tutor assistance

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -16,16 +16,16 @@ const initdb = async () =>
 export const putDb = async (content) => {
   console.error('putDb not implemented');
     // Create a connection to the database database and version we want to use.
-    const todosDb = await openDB('todos', 1);
+    const jateDb = await openDB('jate', 1);
 
     // Create a new transaction and specify the database and data privileges.
-    const tx = todosDb.transaction('todos', 'readwrite');
+    const tx = jateDb.transaction('jate', 'readwrite');
   
     // Open up the desired object store.
-    const store = tx.objectStore('todos');
+    const store = tx.objectStore('jate');
   
     // Use the .add() method on the store and pass in the content.
-    const request = store.add({ todo: content });
+    const request = store.put({ id: 1, jate: content });
   
     // Get confirmation of the request.
     const result = await request;
@@ -39,21 +39,21 @@ console.error('getDb not implemented');
 console.log('GET all from the database');
 
 // Create a connection to the database database and version we want to use.
-const todosDb = await openDB('todos', 1);
+const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-const tx = todosDb.transaction('todos', 'readonly');
+const tx = jateDb.transaction('jate', 'readonly');
 
 // Open up the desired object store.
-const store = tx.objectStore('todos');
+const store = tx.objectStore('jate');
 
 // Use the .getAll() method to get all data in the database.
-const request = store.getAll();
+const request = store.get(1);
 
  // Get confirmation of the request.
 const result = await request;
-console.log('result.value', result);
-return result;
+console.log('jate', result.jate);
+return result.jate;
 };
 
 
