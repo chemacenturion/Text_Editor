783ac46f2e9e7fe15606143eacc17c06ad8625e2
chemacenturion <centurion.philadelphia@gmail.com>
2021-10-16 16:15:14 -0400

    pushing up manifest webpack- needs updating

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -29,6 +29,23 @@ module.exports = () => {
         clientsClaim: true,
         skipWaiting: true,
       }),
+
+      new WebpackPwaManifest({
+        name: 'TODOs',
+        short_name: 'TODOs',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
